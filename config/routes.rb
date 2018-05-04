8b44ef34aa2d3b6162f52593bc026767c523d9a0
Yuyu <qscez2001@gmail.com>
2018-05-04 21:02:39 +0800

    generate routes, action and view for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index]
-  root :to => 'users#index'
+  resources :posts, only: [:index]
+  root :to => 'posts#index'
 
   namespace :admin do
     resources :users, only: [:index]
