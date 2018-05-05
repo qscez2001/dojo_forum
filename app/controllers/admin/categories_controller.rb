52f42ef43dda8af1a6b46ac57d8d6fa92765ec10
Yuyu <qscez2001@gmail.com>
2018-05-05 13:15:03 +0800

    admin can new and destroy categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,34 @@
-class Admin::CategoriesController < ApplicationController
+class Admin::CategoriesController < Admin::BaseController
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+
+    redirect_to admin_categories_path
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to admin_categories_path
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to admin_categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
